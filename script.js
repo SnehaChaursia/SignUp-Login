9149c995583969a4ea948163d6d3174193098b29
SnehaChaursia <chaurasiasneha30@gmail>
2024-12-15 11:47:45 +0530

    added css /login functionality

--- script.js
+++ script.js
@@ -14,8 +14,45 @@ signupbtn.addEventListener('click',()=>{
    }
      if (localStorage.getItem(email)===password){
     alert("Already Registered")
-     return;
-       }
+    login_page.click(); 
+     }
      localStorage.setItem(email,password);
-     
+     login_page.click(); 
 })
+document.getElementById("login_page").addEventListener('click',()=>{
+  document.querySelector('.sigup_page').style.display="none";
+  document.querySelector('.Login_page').style.display="block"
+
+})
+document.getElementById("signup_page").addEventListener('click',()=>{
+  document.querySelector('.sigup_page').style.display="block";
+  document.querySelector('.Login_page').style.display="none"
+
+})
+
+const loginbtn=document.getElementById('loginbtn');
+loginbtn.addEventListener('click',()=>{
+  const user=document.getElementById('mail').value;
+  const password=document.getElementById('passkey').value;
+  if(!user||!password){
+    alert('Please fill details')
+    return;
+  }
+  let savedPAssword=localStorage.getItem(user);
+     if(savedPAssword===password){
+        alert("Successfully login")
+     } else{
+        alert("Icorrect password try again!")
+     }
+     localStorage.setItem("isloggedIn",user);
+})
+
+// add a feature user not registered and redirect to signup page.
+// incorrect username/password
+// let savedPAssword=localStorage.getItem(user);
+// ****if(savedPAssword===password){
+//   alert("Successfully login")
+// } else{
+//   alert("Icorrect password try again!")
+// }
+// localStorage.setItem("isloggedIn",user);**** sir s smjhna hai.
